5a50b8a59d49d81781f7503ba77fd011a1747fea
AlephArcade <shlomofellig@gmail.com>
2025-03-12 15:59:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1623,7 +1623,7 @@ function addTransliterationToggleStyles() {
               <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/>
               <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/>
             </svg>
-            <div class="toggle-indicator">${gameState.showTransliteration ? 'On' : 'Off'}</div>
+            <div class="toggle-indicator"></div>
           </button>
 
         </div>
